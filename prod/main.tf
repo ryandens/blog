c9513f75d44b73ca457e0eb9ec0a59c3bf03827f
Ryan Dens <ryandens735@gmail.com>
2020-05-30 15:13:22 -0400

    :arrow_up: upgrade terraform module

--- prod/main.tf
+++ prod/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 module "my-static-site" {
-  source   = "github.com/ryandens/terraform-aws-personal-modules//modules/static-site?ref=v0.0.1"
+  source   = "github.com/ryandens/terraform-aws-personal-modules//modules/static-site?ref=v0.0.3"
   dns_name = "ryandens.com"
 }
--- prod/outputs.tf
+++ prod/outputs.tf
@@ -2,3 +2,8 @@ output "name_servers" {
   description = "the Route 53 Zone for configuring your domain name provider"
   value       = module.my-static-site.name_servers
 }
+
+output "bucket_id" {
+  value = module.my-static-site.bucket_id
+}
+
